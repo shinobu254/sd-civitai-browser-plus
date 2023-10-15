66c06e8f884b0d8a0479827741da50f3f0460f39
RabuHoteru <41131937+shinobu254@users.noreply.github.com>
2023-10-15 15:14:30 -0400

    Fix Class

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
-import launch
+import neko
 
-if not launch.is_installed("send2trash"):
-    launch.run_pip("install send2trash", "requirements for CivitAI Browser")
-if not launch.is_installed("ZipUnicode"):
-    launch.run_pip("install ZipUnicode", "requirements for CivitAI Browser")
+if not neko.is_installed("send2trash"):
+    neko.run_pip("install send2trash", "requirements for CivitAI Browser")
+if not neko.is_installed("ZipUnicode"):
+    neko.run_pip("install ZipUnicode", "requirements for CivitAI Browser")
